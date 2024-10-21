446558a0a4efd7ac12edbc3b7af29fb47557f984
Lewis Chan <8966669+lwschan@users.noreply.github.com>
2024-10-21 20:44:29 +0800

    update config

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,9 +6,8 @@ plugins {
 }
 
 java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 
 @Suppress("UnstableApiUsage")
