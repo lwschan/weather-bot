3101e688911c68176a3f5959a5e2c913ccbe205b
Lewis Chan <8966669+lwschan@users.noreply.github.com>
2024-10-26 02:12:56 +0800

    undo test change

--- src/test/kotlin/dev/lewischan/weatherbot/model/TemperatureTest.kt
+++ src/test/kotlin/dev/lewischan/weatherbot/model/TemperatureTest.kt
@@ -4,30 +4,26 @@ import io.kotest.core.spec.style.FunSpec
 import io.kotest.datatest.withData
 import io.kotest.matchers.shouldBe
 
-class TemperatureTest : FunSpec() {
-    init {
-        test("") {
-            1 shouldBe 1
-        }
+class TemperatureTest : FunSpec({
 
-        context("Celsius should be converted to Fahrenheit correctly") {
-            withData(
-                30.4 to 86.72,
-                15.0 to 59.0,
-                15.23 to 59.41
-            ) { (celsius, fahrenheit) ->
-                Temperature.celsius(celsius).fahrenheit shouldBe fahrenheit
-            }
-        }
-
-        context("Fahrenheit should be converted to Celsius correctly") {
-            withData(
-                86.72 to 30.4,
-                212.0 to 100.0
-            ) { (fahrenheit, celsius) ->
-                Temperature.fahrenheit(fahrenheit).celsius shouldBe celsius
-
-            }
+    context("Celsius should be converted to Fahrenheit correctly") {
+        withData(
+            30.4 to 86.72,
+            15.0 to 59.0,
+            15.23 to 59.41
+        ) { (celsius, fahrenheit) ->
+            Temperature.celsius(celsius).fahrenheit shouldBe fahrenheit
         }
     }
-}
+
+    context("Fahrenheit should be converted to Celsius correctly") {
+        withData(
+            86.72 to 30.4,
+            212.0 to 100.0
+        ) { (fahrenheit, celsius) ->
+            Temperature.fahrenheit(fahrenheit).celsius shouldBe celsius
+
+        }
+    }
+
+})
