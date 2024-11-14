545d75ef3afd38f1ef88222b01b71d06f3a203de
Lewis Chan <8966669+lwschan@users.noreply.github.com>
2024-11-14 23:38:30 +0800

    remove kotlinx-coroutines-reactor, retrofit2 dependencies that aren't needed

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ java {
     targetCompatibility = JavaVersion.VERSION_21
 }
 
+@Suppress("UnstableApiUsage")
 configurations {
     compileOnly {
         extendsFrom(configurations.annotationProcessor.get())
@@ -49,9 +50,7 @@ dependencies {
     implementation(libs.jackson.module.kotlin)
     implementation(libs.kotlin.reflect)
     implementation(libs.kotlin.telegram.bot)
-    implementation(libs.kotlinx.coroutines.reactor)
     implementation(libs.micrometer.tracing.bridge.brave)
-    implementation(libs.retrofit2)
     implementation(libs.spring.boot.starter.actuator)
     implementation(libs.spring.boot.starter.jdbc)
     implementation(libs.spring.boot.starter.web)
