9aa676cd69af1e55a8163a2e3bf2bd5b9c09451c
Lewis Chan <8966669+lwschan@users.noreply.github.com>
2024-11-18 00:24:33 +0800

    debug bot

--- src/main/kotlin/dev/lewischan/weatherbot/controller/TelegramBotController.kt
+++ src/main/kotlin/dev/lewischan/weatherbot/controller/TelegramBotController.kt
@@ -3,6 +3,8 @@ package dev.lewischan.weatherbot.controller
 import com.github.kotlintelegrambot.entities.Update
 import dev.lewischan.weatherbot.bot.TelegramBot
 import dev.lewischan.weatherbot.configuration.TelegramBotProperties
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -14,11 +16,15 @@ class TelegramBotController(
     val telegramBot: TelegramBot
 ) {
 
+    val logger: Logger = LoggerFactory.getLogger(javaClass)
+
     @PostMapping("/{botApiToken}")
     suspend fun webhook(
         @PathVariable botApiToken: String,
         @RequestBody update: Update
     ): ResponseEntity<Unit> {
+        logger.info("Handling webhook request for Telegram")
+
         if (botApiToken != telegramBotProperties.apiToken) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build()
         }
