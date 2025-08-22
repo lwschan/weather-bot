ce7d7d3bb23c48a4c0d7e13e2561e5a4b94a5efd
Lewis Chan <8966669+lwschan@users.noreply.github.com>
2025-08-22 22:18:28 +0800

    update deps, fix test warnings

--- build.gradle.kts
+++ build.gradle.kts
@@ -106,7 +106,10 @@ dependencies {
 
 kotlin {
     compilerOptions {
-        freeCompilerArgs.addAll("-Xjsr305=strict")
+        freeCompilerArgs.addAll(
+            "-Xjsr305=strict",
+            "-Xannotation-default-target=param-property"
+        )
     }
 }
 
