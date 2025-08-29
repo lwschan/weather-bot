e5983d16f348752f8a9ad5190b26e58778ef02ea
Lewis Chan <8966669+lwschan@users.noreply.github.com>
2025-08-29 21:23:14 +0800

    fix kotest breaking changes

--- src/test/kotlin/dev/lewischan/weatherbot/KotestProjectConfig.kt
+++ src/test/kotlin/dev/lewischan/weatherbot/KotestProjectConfig.kt
@@ -1,18 +1,16 @@
 package dev.lewischan.weatherbot
 
 import io.kotest.core.config.AbstractProjectConfig
-import io.kotest.core.extensions.Extension
 import io.kotest.core.test.AssertionMode
-import io.kotest.extensions.spring.SpringAutowireConstructorExtension
+import io.kotest.engine.concurrency.TestExecutionMode
 import io.kotest.extensions.spring.SpringExtension
 
 class KotestProjectConfig : AbstractProjectConfig() {
 
-    override val parallelism: Int = 1
+    override val testExecutionMode = TestExecutionMode.Sequential
     override val assertionMode = AssertionMode.Error
 
-    override fun extensions(): List<Extension> = super.extensions() + listOf(
-        SpringExtension,
-        SpringAutowireConstructorExtension
+    override val extensions = super.extensions + listOf(
+        SpringExtension()
     )
 }
