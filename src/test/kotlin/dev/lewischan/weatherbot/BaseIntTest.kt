6d10c3a2ce53a656e8cb52fae803796e9f6ea2ce
Lewis Chan <8966669+lwschan@users.noreply.github.com>
2025-11-23 17:37:07 +0800

    fix merge

--- src/test/kotlin/dev/lewischan/weatherbot/BaseIntTest.kt
+++ src/test/kotlin/dev/lewischan/weatherbot/BaseIntTest.kt
@@ -1,7 +1,5 @@
 package dev.lewischan.weatherbot
 
-import io.kotest.core.spec.style.FunSpec
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.webtestclient.autoconfigure.AutoConfigureWebTestClient
 import org.springframework.test.context.ActiveProfiles
