1266ef1adae7d39c626d4dce54b6160642dd84c7
Lewis Chan <8966669+lwschan@users.noreply.github.com>
2026-05-17 16:05:47 +0800

    fix: enable automatic context propagation

--- src/main/kotlin/dev/lewischan/weatherbot/WeatherBotApplication.kt
+++ src/main/kotlin/dev/lewischan/weatherbot/WeatherBotApplication.kt
@@ -2,10 +2,12 @@ package dev.lewischan.weatherbot
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import reactor.core.publisher.Hooks
 
 @SpringBootApplication
 class WeatherBotApplication
 
 fun main(args: Array<String>) {
+    Hooks.enableAutomaticContextPropagation()
     runApplication<WeatherBotApplication>(*args)
 }
