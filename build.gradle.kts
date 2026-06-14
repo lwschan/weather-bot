8096049c20c30b6c3eca51c3e30dae6ce5da8737
Lewis Chan <8966669+lwschan@users.noreply.github.com>
2026-06-14 12:24:48 +0100

    chore: make Kotlin version the same as the only Spring Boot bundles

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ plugins {
     alias(libs.plugins.spring.boot)
     alias(libs.plugins.spring.dependency.management)
     jacoco
-    kotlin("jvm") version libs.versions.kotlin.jvm
-    kotlin("plugin.spring") version libs.versions.kotlin.plugin.spring
+    kotlin("jvm")
+    kotlin("plugin.spring")
 }
 
 java {
